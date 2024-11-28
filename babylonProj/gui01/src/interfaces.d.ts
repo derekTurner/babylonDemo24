e09c49548f0da8bcdad27ca3f055f55da23196b7
derekTurner <derek.turner@uws.ac.uk>
2024-11-28 16:29:29 +0000

    gui 04

--- babylonProj/gui01/src/interfaces.d.ts
+++ babylonProj/gui01/src/interfaces.d.ts
@@ -1,21 +1,21 @@
 import {
-    Scene,
-    Mesh,
-    HemisphericLight,
-    Camera,
-    GroundMesh,
-  } from "@babylonjs/core";
+  Scene,
+  Mesh,
+  HemisphericLight,
+  Camera,
+  GroundMesh,
+} from "@babylonjs/core";
 
-  import {Button }  from "@babylonjs/gui/2D";
+import {Button }  from "@babylonjs/gui/2D";
 
-  export interface SceneData {
-    scene: Scene;
-    ground:GroundMesh;
-    sky: Mesh;
-    lightHemispheric: HemisphericLight;
-    camera: Camera;
-  }
+export interface SceneData {
+  scene: Scene;
+  ground:GroundMesh;
+  sky: Mesh;
+  lightHemispheric: HemisphericLight;
+  camera: Camera;
+}
 
-  export interface GUIData {
-    button1:Button;
-  }
+export interface GUIData {
+  button1:Button;
+}
